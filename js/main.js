07fd47b21ad3a3b6f2f889f70f52bc2e557a5248
Cory McCartan <mccartan@grinnell.edul>
2015-09-25 16:28:28 -0500

    improved drop down menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 function main() {
+	els = $$(".mobile .dropdown h3");
 
+	for (var i = 0; i < els.length; i++) {
+		var el = els[i];
+		el.onclick = openSubMenu.bind(null, el.innerHTML);
+	}	
+}
+
+function openSubMenu(name) {
+	// hide all other submenus
+	var menus = $$(".subdrop");
+
+	for (var i = 0; i < menus.length; i++) {
+		var menu = menus[i];
+		menu.style.display = "none";
+	}
+
+	// show correct menu
+	$(".subdrop#s_" + name.toLowerCase()).style.display = "block";
 }
